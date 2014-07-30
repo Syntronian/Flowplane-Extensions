c4a2d3f151c465177976b621cd51d89c44b98564
Steve Shearn <steve@syntronian.com>
2014-07-30 17:23:49 +1000

    visual studio missed this

---
+++ source/Extensions/ExtensionsCore/Definitions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionsCore
+{
+    public class Definitions
+    {
+        public enum ProcessObjectTypes
+        {
+            Activity = 1,
+            Event = 2
+        }
+    }
+}
